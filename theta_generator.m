e5e14eb4ef8330cd3baf83d42650c028cc445088
Austin Warren <austin@ugls14.ece.uvic.ca>
2014-06-23 13:54:57 -0700

    Added comment into theta gen

--- theta_generator.m
+++ theta_generator.m
@@ -7,5 +7,5 @@ for i=0:31 %For hyperbolic: 0:31 --> 1:32
     [~,e] = log2(t);
     s = dec2bin(t,max(30,e));
     %s = [s(1:end-30),'.',s(end-30+1:end)];
-    fprintf('%i => "00%s",\n',i,s);
+    fprintf('%i => "00%s", --Delta: %i Theta: %i \n',i,s,delta,theta);
 end
