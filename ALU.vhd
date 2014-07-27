3235ccd25e7684b869464ce2488f7d1c14759f73
CORDICGIT <kyle.treloar@gmail.com>
2014-07-27 14:45:27 -0700

    Added Comments

--- ALU.vhd
+++ ALU.vhd
@@ -30,21 +30,19 @@ use IEEE.NUMERIC_STD.ALL;
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
---Testing Wilson
---Comment added by austin warren
 
 entity ALU is
 
-port(	inX : IN  std_logic_vector(31 downto 0);
-		inY : IN  std_logic_vector(31 downto 0);
-		inZ : IN  std_logic_vector(31 downto 0);
+port(	inX : IN  std_logic_vector(31 downto 0); -- in X
+		inY : IN  std_logic_vector(31 downto 0); -- in Y
+		inZ : IN  std_logic_vector(31 downto 0); -- in Z
 		i: IN std_logic_vector(4 downto 0); -- iteration number
-		theta : in std_logic_vector(31 downto 0);
-		m: in std_logic_vector(1 downto 0);
+		theta : in std_logic_vector(31 downto 0); -- in theta value from look up table
+		m: in std_logic_vector(1 downto 0); -- current mode 
 		addSub:	in std_logic; -- add or subtract addSub. addSub '0' x and z are addition, y is subtraction. addSub '1' is vice versa
-      result_X : OUT  std_logic_vector(31 downto 0);
-      result_Y : OUT  std_logic_vector(31 downto 0);
-      result_Z : OUT  std_logic_vector(31 downto 0)
+      result_X : OUT  std_logic_vector(31 downto 0); -- out x
+      result_Y : OUT  std_logic_vector(31 downto 0); -- out y
+      result_Z : OUT  std_logic_vector(31 downto 0) -- out z
 );
 
 end ALU;
@@ -53,7 +51,7 @@ architecture behv of ALU is
 begin			
 		   
 	 
-    pX : process(i,inX,inY,inZ,theta, addSub) is
+    pXYZ : process(i,inX,inY,inZ,theta, addSub) is
 	 variable signed_X,signed_Y: signed(31 downto 0);
 	 variable iteration :integer;
 	 variable signed_Z: signed(31 downto 0);
@@ -61,12 +59,35 @@ begin
     
 	-- use case statement to achieve 
 	-- different operations of ALU
+	
+	--  signed variables are created in order to use the mathematical operations from NUMERIC library.
 		signed_X := signed(inX);
 		signed_Y := signed(inY);
-		iteration := to_integer(unsigned(i)); 
-		if m = "00" then
+		signed_Z := signed(inZ);
+		iteration := to_integer(unsigned(i));
+
+--Circular mode m = 1
+--Linear mode m = 0
+--Hyperbolic mode m = -1
+--
+--Vectoring operation y => 0
+--Rotation operation z => 0
+--
+--delta = 2^(-i) => from lookup table
+--theta => lookup table
+--
+--generalized cordic
+--xi+1 = xi - (m) * (Mew)i * (y)i * (delta)i
+--yi+1 = yi + (Mew)i * (x)i * (delta)i
+--zi+1 = zi - (Mew)i * (theta)i 
+--
+--binary point is after the second MSB (sign bit, bit, binary point, frac bit, frac bit, frac bit....)
+--'00' is linear, '01' is circular, '10' hyperbolic, '11' is nothing
+
+		-- result_X ALU calculation
+		if m = "00" then -- linear mode, x value does not change m = 0 for generalized cordic
 			result_X <= inX;
-		elsif m = "01" then
+		elsif m = "01" then -- circular mode, m = 1 for generalized cordic
 			case addSub is
 			when '0' =>
 				result_X <= std_logic_vector(signed_X + shift_right(signed_Y,iteration)); -- Addition of Input 1 and Input 2
@@ -75,57 +96,39 @@ begin
 			when others =>	 
 				result_X <= x"00000000"; -- nop
 			end case;
-		elsif m = "10" then
+		elsif m = "10" then -- hyperbolic mode, m = -1 for generalized cordic
 			case addSub is
 			when '1' =>
 				result_X <= std_logic_vector(signed_X + shift_right(signed_Y,iteration)); -- Addition of Input 1 and Input 2
 			when '0' =>						
 				result_X <= std_logic_vector(signed_X - shift_right(signed_Y,iteration)); -- 2's compliment subtraction of Input 1 and Input 2
 			when others =>	 
-				result_X <= x"00000000"; -- nop
+				result_X <= x"00000000"; -- nop, value should not occur and is an error state. Occurs if m = "11"
 			end case;
 		else
-			result_X <= inX;
+			result_X <= inX; -- nop, error state.
 		end if;
 
-		
---    end process pX;
-	 
---	 pY : process(theta) is
---	 variable signed_X,signed_Y: signed(31 downto 0);
---	 variable iteration :integer;
---    begin
-    
-	-- use case statement to achieve 
-	-- different operations of ALU
---			signed_X := signed(inX);
---			signed_Y := signed(inY);
---			iteration := to_integer(unsigned(i)); 
-				case addSub is
-					 when '1' =>
-						result_Y <= std_logic_vector(signed_Y + shift_right(signed_X,iteration)); -- Addition of Input 1 and Input 2
-					 when '0' =>						
-						result_Y <= std_logic_vector(signed_Y - shift_right(signed_X,iteration)); -- 2's compliment subtraction of Input 1 and Input 2
-					 when others =>	 
-						result_Y <= x"00000000"; -- nop
-					  end case;
-    --end process pX;
+		-- result_Y ALU calculation
+		case addSub is
+			 when '1' =>
+				result_Y <= std_logic_vector(signed_Y + shift_right(signed_X,iteration)); -- Addition of Input 1 and Input 2
+			 when '0' =>						
+				result_Y <= std_logic_vector(signed_Y - shift_right(signed_X,iteration)); -- 2's compliment subtraction of Input 1 and Input 2
+			 when others =>	 
+				result_Y <= x"00000000"; -- nop, error state.
+			  end case;
 
-   -- pZ : process(theta, addSub) is
-	 --variable signed_Z: signed(31 downto 0);
-  --  begin
-	-- use case statement to achieve 
-	-- different operations of ALU
-			signed_Z := signed(inZ);
-				case addSub is
-					 when '0' =>
-						result_Z <= std_logic_vector(signed_Z + signed(theta)) ; -- Addition of Input 1 and Input 2
-					 when '1' =>						
-						result_Z <= std_logic_vector(signed_Z - signed(theta)); -- 2's compliment subtraction of Input 1 and Input 2
-					 when others =>	 
-						result_Z <= x"00000000"; -- nop
-					  end case;
-    end process pX;
+		-- result_Z ALU calculation
+		case addSub is
+			 when '0' =>
+				result_Z <= std_logic_vector(signed_Z + signed(theta)) ; -- Addition of Input 1 and Input 2
+			 when '1' =>						
+				result_Z <= std_logic_vector(signed_Z - signed(theta)); -- 2's compliment subtraction of Input 1 and Input 2
+			 when others =>	 
+				result_Z <= x"00000000"; -- nop, error state.
+			  end case;
+    end process pXYZ;
 
 
 end behv;
